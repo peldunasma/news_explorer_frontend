1524265dded64e661cac73be8e51d4b20339964a
Matthew Peldunas <peldunasma@gmail.com>
2024-07-15 13:21:00 -0400

    new

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,19 +2,9 @@
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
-import RegisterPopup from "../RegisterPopup/RegisterPopup";
 import Footer from "../Footer/Footer";
 
 function App() {
-  // const [activeModal, setActiveModal] = useState("");
-
-  // function handleCreatePopup() {
-  //   setActiveModal("create");
-  // }
-
-  // function handleClosePopup() {
-  //   setActiveModal("");
-  // }
 
   return (
     <>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,13 +9,13 @@ const Header = () => {
       className="header_background-image"
       alt="Background Image"
       />
-      <nav className header__navBar>
+      <nav className="header__navBar">
         <h2 className="header_navBar-title">News Explorer</h2>
         <div className="header_navBar-buttons">
-          <button type="text className=" header_navBar-button-home>
+          <button type="text" className="header_navBar-button-home">
             Home
           </button>
-          <button type="text className=" header_navBar-button-login>
+          <button type="text" className="header_navBar-button-login">
             Sign In
           </button>
         </div>
