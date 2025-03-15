c2e4cc9f05d0e369a0a50f9712705fba77d969d2
Matthew Peldunas <peldunasma@gmail.com>
2025-03-15 13:41:39 -0400

    revised

--- src/components/RegisterModal/RegisterModal.jsx
+++ src/components/RegisterModal/RegisterModal.jsx
@@ -77,14 +77,14 @@ const RegisterModal = ({
       </label>
       {isValid ? (
       <button 
-      className="modal_submit-button button_enabled" 
+      className="modal_submit-button-enabled" 
       type="submit"
       >
         Sign Up
       </button>
       ) : (
       <button 
-      className="modal_submit-button button_disabled"
+      className="modal_submit-button-disabled"
       type="submit"
       >
         Sign Up
